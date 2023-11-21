9ca15d7c672bc0edd980b413b329613d94539235
ShreyasParameshwaran <116908037+ShreyasParameshwaran@users.noreply.github.com>
2023-11-21 11:58:54 +0530

    Add files via upload

---
+++ codesoft_tt_ai/constants.py
@@ -0,0 +1,27 @@
+# ---------
+# CONSTANTS
+# ---------
+
+# --- PIXELS ---
+
+WIDTH = 600
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- COLORS ---
+
+BG_COLOR = (219, 232, 120)
+LINE_COLOR = (23, 145, 135)
+CIRC_COLOR = (250, 97, 71)
+CROSS_COLOR = (66, 66, 66)
